ad30e4d4315c92341248e428fa0504b3669579e4
Toan Tran <tqtran1110@gmail.com>
2023-10-26 13:22:00 -0400

    top post algorithms

--- campuswire-analytics-app/app/mongodb/page.js
+++ campuswire-analytics-app/app/mongodb/page.js
@@ -7,7 +7,8 @@ export default async function Mongo() {
     "mongodb+srv://team8s:rattigan320fa23@campuswire.x730pf7.mongodb.net/";
   const client = new MongoClient(url);
 
-  let results;
+  let results = [];
+
   try {
     // connect to mongoDB
     await client.connect();
@@ -17,11 +18,26 @@ export default async function Mongo() {
     const collection = client.db("posts").collection("2022-12-15");
 
     // find all posts from September with viewsCount > 100
-    const cursor = collection.find({publishedAt: {$regex: '2022-09'}, viewsCount: {$gt: 100}}).sort({viewsCount:-1});
+    const cursor = collection
+      .find({ publishedAt: { $regex: "2022-09" }, viewsCount: { $gt: 100 } })
+      .sort({ viewsCount: -1 });
     results = await cursor.toArray();
     // console.log(results);
+
+    results = results
+      .map((post) => ({
+        ...post,
+        score:
+          post.totalViews +
+          2 * (post.totalViews - post.uniqueViews) +
+          50 * post.totalComments +
+          5 * post.totalLikes -
+          (30 * (new Date() - new Date(post.publishedAt))) /
+            (1000 * 60 * 60 * 24),
+      }))
+      .sort((a, b) => b.score - a.score);
   } catch (e) {
-    console.log("There was an error in connecting to mongo")
+    console.log("There was an error in connecting to mongo");
     console.error(e);
   } finally {
     await client.close();
@@ -31,13 +47,19 @@ export default async function Mongo() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Results from query for 3 most popular posts from September:</div>
       <div>
-        {results.filter((res, i)=> i<3).map((res, i) => (
-          <div key={i}>
-            <p className="m-3">{i + 1}. Title: &quot;{res.title}&quot; ({res.viewsCount} views)</p>
-            <p className="m-3">Views: {res.viewsCount}</p>
-            <p className="m-3">Date published: {res.publishedAt.substr(0,10)}</p>
-          </div>
-        ))}
+        {results
+          .filter((res, i) => i < 3)
+          .map((res, i) => (
+            <div key={i}>
+              <p className="m-3">
+                {i + 1}. Title: &quot;{res.title}&quot; ({res.viewsCount} views)
+              </p>
+              <p className="m-3">Views: {res.viewsCount}</p>
+              <p className="m-3">
+                Date published: {res.publishedAt.substr(0, 10)}
+              </p>
+            </div>
+          ))}
       </div>
     </main>
   );
--- campuswire-analytics-app/app/page.js
+++ campuswire-analytics-app/app/page.js
@@ -1,103 +1,121 @@
 // import styles from "./page.module.css";
 import { MongoClient } from "mongodb";
 import Image from "next/image";
-import goldstar_nobackground from '../public/goldstar_nobackground.png';
-import silverstar_nobackground from '../public/silverstar_nobackground.png';
-import bronzestar_nobackground from '../public/bronzestar_nobackground.png';
+import goldstar_nobackground from "../public/goldstar_nobackground.png";
+import silverstar_nobackground from "../public/silverstar_nobackground.png";
+import bronzestar_nobackground from "../public/bronzestar_nobackground.png";
 
-function cutoff_string(name_to_cut){
-  if (name_to_cut.length < 20){
+function cutoff_string(name_to_cut) {
+  if (name_to_cut.length < 20) {
     return name_to_cut;
-  }
-  else{//name is too long for bubble
-    while(name_to_cut.length >= 20){
-      name_to_cut = name_to_cut.slice(0,name_to_cut.lastIndexOf(' '));
+  } else {
+    //name is too long for bubble
+    while (name_to_cut.length >= 20) {
+      name_to_cut = name_to_cut.slice(0, name_to_cut.lastIndexOf(" "));
     }
-    return name_to_cut+"...";
+    return name_to_cut + "...";
   }
 }
 
-function ranking_match(index){
-  if(index == 0){
+function ranking_match(index) {
+  if (index == 0) {
     return goldstar_nobackground;
-  }
-  else if(index == 1){
+  } else if (index == 1) {
     return silverstar_nobackground;
-  }
-  else if(index == 2){
+  } else if (index == 2) {
     return bronzestar_nobackground;
   }
 }
 
-function find_prolific_users(data){
-  //
+function computePostScore(post) {
+  const w1 = 1,
+    w2 = 2,
+    w3 = 50,
+    w4 = 5,
+    w5 = 30;
+  const today = new Date();
+  const postDate = new Date(post.publishedAt); // Assuming publishedAt is a date string.
+  const daysOld = Math.ceil((today - postDate) / (1000 * 60 * 60 * 24));
+
+  // Use totalViews - uniqueViews for repeated views.
+  const repeatedViews = post.totalViews - post.uniqueViews;
+
+  return (
+    w1 * post.totalViews +
+    w2 * repeatedViews +
+    w3 * post.totalComments +
+    w4 * post.totalLikes -
+    w5 * daysOld
+  );
 }
 
-export default async function Mongo() {
-  // initialize mongoclient credentials
+function findProlificUsers(data) {
+  let userCounts = {};
+
+  data.forEach((post) => {
+    if (post.author) {
+      if (!userCounts[post.author]) {
+        userCounts[post.author] = 0;
+      }
+      userCounts[post.author]++;
+    }
+  });
+
+  return Object.entries(userCounts)
+    .sort(([, aCount], [, bCount]) => bCount - aCount)
+    .map(([author]) => author)
+    .slice(0, 3);
+}
+
+export default function Mongo({ results }) {
+  return (
+    <main>
+      <div className="toppostsbox">
+        <div className="boxtitle">Top Posts</div>
+        <div>
+          {results
+            .filter((res, i) => i < 3)
+            .map((res, i) => (
+              <div className="rankings" key={i}>
+                <div className="medal">
+                  <Image src={ranking_match(i)} />
+                </div>
+                <p className="littlebox">{cutoff_string(res.title)}</p>
+              </div>
+            ))}
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export async function getServerSideProps() {
   const url =
     "mongodb+srv://team8s:rattigan320fa23@campuswire.x730pf7.mongodb.net/";
   const client = new MongoClient(url);
 
   let results;
-  let prolific_users;
+
   try {
-    // connect to mongoDB
     await client.connect();
     console.log("Connected to MongoDB");
-
-    // get collection "2022-12-15" from database "posts" gives access to all the posts
     const collection = client.db("posts").collection("2022-12-15");
-
-    // find all posts from September with viewsCount > 100
-    // use *
-    const cursor = collection.find({publishedAt: {$regex: '2022-11'}, viewsCount: {$gt: 100}}).sort({viewsCount:-1});
+    const cursor = collection
+      .find({ publishedAt: { $regex: "2022-11" }, viewsCount: { $gt: 100 } })
+      .sort({ viewsCount: -1 });
     results = await cursor.toArray();
-    // console.log(results);
-
-    //find most prolific users from September
-    const users_data = collection.find({});
-    prolific_users = await users_data.toArray();
+    results.forEach((post) => (post.score = computePostScore(post)));
+    results.sort((a, b) => b.score - a.score);
   } catch (e) {
-    console.log("There was an error in connecting to mongo")
+    console.log("There was an error in connecting to mongo");
     console.error(e);
   } finally {
     await client.close();
   }
 
-  return (
-    <main>
-      <div className='prolificusersbox'>
-        <div className='boxtitle'>
-        Most Active Users
-        </div>
-        <div>
-          {prolific_users.filter((res, i)=> i<3).map((res, i) => (
-          <div className="rankings" key = {i}>
-            <div className="medal">
-            <Image src={ranking_match(i)}/>  
-            </div>
-            <p className='littlebox'>{cutoff_string(res.title)}</p>
-          </div>
-        ))}
-        </div>
-      </div>
-
-      <div className='toppostsbox'>
-        <div className='boxtitle'>
-        Top Posts
-        </div>
-        <div>
-          {results.filter((res, i)=> i<3).map((res, i) => (
-          <div className="rankings" key = {i}>
-            <div className="medal">
-            <Image src={ranking_match(i)}/>  
-            </div>
-            <p className='littlebox'>{cutoff_string(res.title)}</p>
-          </div>
-        ))}
-        </div>
-      </div>
-    </main>
-  );
+  return {
+    props: {
+      results: results || [],
+    },
+  };
 }
