fc152b464b13abd50a984b546d79ae929ec2232f
Nathan G <njgrab135@gmail.com>
2023-10-28 21:46:15 -0400

    Implemented NLP keyword extraction and began formatting and styling the frontend
    - Utilized a Python NLP keyword extraction library called Yake! to generate a ranked list of keywords/phrases from title and body texts of posts
    - Began formatting and styling the frontend according to the mock-up
    - Created a flexible <Feature> component for the Overview tab that allows for easy scaling of the design

---
+++ campuswire-analytics-app/app/keyword_extractor.py
@@ -0,0 +1,17 @@
+import sys
+import json
+import yake
+kw_extractor = yake.KeywordExtractor(
+  lan = "en",
+  n = 3,
+  dedupLim = 0.25,
+  dedupFunc = 'seqm',
+  windowsSize = 1,
+  top = 15
+)
+keywords = kw_extractor.extract_keywords(sys.argv[1])
+output = []
+for obj in keywords:
+    output.append(obj[0])
+print(json.dumps(output))
+sys.stdout.flush()
