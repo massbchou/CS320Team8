caa337fa2ada59cb4b5e26a7fe187ee66bff22be
Nathan G <njgrab135@gmail.com>
2023-11-05 12:51:33 -0500

    Added caching technology for trending topics
    Added caching for trending topics such that when the top keywords for a new collection date are generated, it stores the result in the database called 'caching' under the collection 'trending topics'.  When that date is accessed again, instead of generating them, it just fetches the corresponding value from the database.

--- campuswire-analytics-app/app/page.js
+++ campuswire-analytics-app/app/page.js
@@ -1,130 +1,87 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from 'next/image';
+import Feature from './feature.js';
+import background from './images/background.png'
 
-export default function Home() {
+export default async function Home() {
+
+  const { MongoClient } = require('mongodb');
+  const uri = 'mongodb+srv://team8s:rattigan320fa23@campuswire.x730pf7.mongodb.net/?retryWrites=true&w=majority';
+  const client = new MongoClient(uri);
+  // Set up MongoClient connection class
+
+  let topPhrases;
+
+  let inputText = '';
+  // Create initially empty input text variable
+
+  try{
+    await client.connect();
+    // Connect to cluster
+
+    let collectionDate = '2022-12-15';
+    // Set collection date
+
+    let cacheCollection = client.db('caching').collection('trending topics');
+    let cache = await cacheCollection.findOne({collectionDate: collectionDate});
+
+    if(cache === null){
+      const MAX_DAYS_OLD = 20;
+      let comparisonDate = new Date(new Date(collectionDate).getTime() - (1000 * 60 * 60 * 24 * MAX_DAYS_OLD)).toISOString();
+      // Create a new date string that represents a date MAX_DAYS_OLD days in the past in relation to the collection date
+
+      let cursor = client.db('posts').collection(collectionDate).find({publishedAt: {$gt: comparisonDate}});
+      // Finds all posts made within the past MAX_DAYS_OLD days in relation to the collection date
+
+      let arr = await cursor.toArray();
+      // Turn cursor into an array of post objects
+
+      arr = arr.filter((x) => !(x.body.substring(0, 3) === 'zzz'));
+      // Filter out all fake entries that start with 'zzz'
+
+      arr.forEach(function(x){
+        if(x.body.indexOf('![') >= 0){
+          let start = Math.max(x.body.lastIndexOf('.png)'), x.body.lastIndexOf('.jpg)')) + 5;
+          x.body = x.body.substring(0, x.body.indexOf('![')) + x.body.substring(start);
+        }
+      });
+      // Remove all embedded images
+      
+      for(let i = 0; i < arr.length; i++){
+        inputText += arr[i].title + ' ' + arr[i].body + ' ';
+      }
+      // Generate concatenation of all remaining title and body texts
+    
+      inputText = inputText.replaceAll('\n', ' ');
+      // Remove all newlines
+
+      const { PythonShell } = require('python-shell');
+
+      await PythonShell.run('./app/keyword_extractor.py', {mode: 'json', pythonPath: 'py', args: [inputText]}).then(msg => {
+        topPhrases = msg[0];
+      });
+      // Spawn Python process, pass it the text
+      // When the output from the script is receieved, capture it
+
+      await cacheCollection.insertOne({collectionDate: collectionDate, topPhrases: topPhrases});
+    }else{
+      topPhrases = cache.topPhrases;
+    }
+  } catch(e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main style={{backgroundImage: `url(${background.src})`, backgroundSize: '100%', backgroundRepeat: 'no-repeat', height:'100vh'}}>
+      <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+        <Image src='/images/icon.png' width={90} height={90} quality={100} style={{margin:'10px'}} unoptimized></Image>
+        <span style={{fontFamily:'Roboto', textAlign:'center', fontSize:'30px'}}>Campuswire Analytics</span>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-
-        <a
-          href="/mongodb"
-          className="group rounded-lg border border-blue-700 px-5 py-4 transition-colors hover:border-2 hover:border-gray-300 hover:bg-gray-100 hover:dark:border-sky-500 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            MongoDB!!!{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Take a look at the backend!!!
-          </p>
-        </a>
+      <div style={{display:'flex', justifyContent:'center', alignItems:'center', margin: '10px'}}>
+        <Feature linkTo='trending-topics' title='Trending Topics' content={topPhrases}></Feature>
+        <Feature linkTo='top-posts' title='Top Posts' content={['Post #1', 'Post #2', 'Post #3', 'Post #4', 'Post #5']}></Feature>
+        <Feature linkTo='most-active-users' title='Most Active Users' content={['User #1', 'User #2', 'User #3', 'User #4', 'User #5']}></Feature>
       </div>
     </main>
   )
