2983fb80e994e19b23dcbcc56140d4b59db57b87
Nathan G <njgrab135@gmail.com>
2023-11-05 12:27:11 -0500

    Updated database query and added caching technology
    Updated the database query such that it only considers posts made within the past x amount of days for the trending topics algorithm
    Added caching for trending topics such that when the top keywords for a new collection date are generated, it stores the result in the database called 'caching' under the collection 'trending topics'.  When that date is accessed again, instead of generating them, it just fetches the corresponding value from the database.

--- campuswire-analytics-app/app/page.js
+++ campuswire-analytics-app/app/page.js
@@ -9,6 +9,8 @@ export default async function Home() {
   const client = new MongoClient(uri);
   // Set up MongoClient connection class
 
+  let topPhrases;
+
   let inputText = '';
   // Create initially empty input text variable
 
@@ -16,45 +18,59 @@ export default async function Home() {
     await client.connect();
     // Connect to cluster
 
-    let cursor = await client.db('posts').collection('2022-12-15').find({}).limit(150);
-    // Find the first x posts on this day, according to the set limit
+    let collectionDate = '2022-12-15';
+    // Set collection date
 
-    let arr = await cursor.toArray();
-    // Turn cursor into an array of post objects
+    let cacheCollection = client.db('caching').collection('trending topics');
+    let cache = await cacheCollection.findOne({collectionDate: collectionDate});
 
-    arr = arr.filter((x) => !(x.body.substring(0, 3) === 'zzz'));
-    // Filter out all fake entries that start with "zzz"
+    if(cache === null){
+      const MAX_DAYS_OLD = 20;
+      let comparisonDate = new Date(new Date(collectionDate).getTime() - (1000 * 60 * 60 * 24 * MAX_DAYS_OLD)).toISOString();
+      // Create a new date string that represents a date MAX_DAYS_OLD days in the past in relation to the collection date
 
-    arr.forEach(function(x){
-      if(x.body.indexOf("![") >= 0){
-        let start = Math.max(x.body.lastIndexOf('.png)'), x.body.lastIndexOf('.jpg)')) + 5;
-        x.body = x.body.substring(0, x.body.indexOf('![')) + x.body.substring(start);
+      let cursor = client.db('posts').collection(collectionDate).find({publishedAt: {$gt: comparisonDate}});
+      // Finds all posts made within the past MAX_DAYS_OLD days in relation to the collection date
+
+      let arr = await cursor.toArray();
+      // Turn cursor into an array of post objects
+
+      arr = arr.filter((x) => !(x.body.substring(0, 3) === 'zzz'));
+      // Filter out all fake entries that start with 'zzz'
+
+      arr.forEach(function(x){
+        if(x.body.indexOf('![') >= 0){
+          let start = Math.max(x.body.lastIndexOf('.png)'), x.body.lastIndexOf('.jpg)')) + 5;
+          x.body = x.body.substring(0, x.body.indexOf('![')) + x.body.substring(start);
+        }
+      });
+      // Remove all embedded images
+      
+      for(let i = 0; i < arr.length; i++){
+        inputText += arr[i].title + ' ' + arr[i].body + ' ';
       }
-    });
-    // Remove all embedded images
+      // Generate concatenation of all remaining title and body texts
     
-    for(let i = 0; i < arr.length; i++){
-      inputText += arr[i].title + ' ' + arr[i].body + ' ';
-    }
-    // Generate concatenation of all remaining title and body texts
-  
-    inputText = inputText.replaceAll('\n', ' ');
-    // Remove all newlines
+      inputText = inputText.replaceAll('\n', ' ');
+      // Remove all newlines
 
+      const { PythonShell } = require('python-shell');
+
+      await PythonShell.run('./app/keyword_extractor.py', {mode: 'json', pythonPath: 'py', args: [inputText]}).then(msg => {
+        topPhrases = msg[0];
+      });
+      // Spawn Python process, pass it the text
+      // When the output from the script is receieved, capture it
+
+      await cacheCollection.insertOne({collectionDate: collectionDate, topPhrases: topPhrases});
+    }else{
+      topPhrases = cache.topPhrases;
+    }
   } catch(e) {
     console.error(e);
   } finally {
     await client.close();
   }
-
-  const { PythonShell } = require('python-shell');
-
-  let topPhrases = ['Phrase 1', 'Phrase 2'];
-  /*await PythonShell.run('./app/keyword_extractor.py', {mode: 'json', pythonPath: 'py', args: [inputText]}).then(msg => {
-    topPhrases = msg[0];
-  });*/
-  // Spawn Python process, pass it the text
-  // When the output from the script is receieved, capture it
   
   return (
     <main style={{backgroundImage: `url(${background.src})`, backgroundSize: '100%', backgroundRepeat: 'no-repeat', height:'100vh'}}>
@@ -63,9 +79,9 @@ export default async function Home() {
         <span style={{fontFamily:'Roboto', textAlign:'center', fontSize:'30px'}}>Campuswire Analytics</span>
       </div>
       <div style={{display:'flex', justifyContent:'center', alignItems:'center', margin: '10px'}}>
-        <Feature linkTo= 'trending-topics' title='Trending Topics' content={topPhrases}></Feature>
-        <Feature linkTo= 'top-posts' title='Top Posts' content={['Post #1', 'Post #2', 'Post #3', 'Post #4', 'Post #5']}></Feature>
-        <Feature linkTo= 'most-active-users' title='Most Active Users' content={['User #1', 'User #2', 'User #3', 'User #4', 'User #5']}></Feature>
+        <Feature linkTo='trending-topics' title='Trending Topics' content={topPhrases}></Feature>
+        <Feature linkTo='top-posts' title='Top Posts' content={['Post #1', 'Post #2', 'Post #3', 'Post #4', 'Post #5']}></Feature>
+        <Feature linkTo='most-active-users' title='Most Active Users' content={['User #1', 'User #2', 'User #3', 'User #4', 'User #5']}></Feature>
       </div>
     </main>
   )
