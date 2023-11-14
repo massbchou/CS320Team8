669dfa7f78481058971d7d8c4f621d7adfbed5c8
lindsayydevine <99309612+lindsayydevine@users.noreply.github.com>
2023-11-13 20:59:59 -0500

    Got rid of the background image

--- campuswire-analytics-app/app/top-posts/page.js
+++ campuswire-analytics-app/app/top-posts/page.js
@@ -216,7 +216,7 @@ export default async function Page() {
   return (
     <main
       style={{
-        backgroundImage: `url(${background.src})`,
+        background: "radial-gradient(ellipse at center top, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 50%, rgba(255,255,255,0) 100%), linear-gradient(140deg, rgba(240, 56, 255, .5) 0%, rgba(255,255,255, .5) 50%, rgba(0, 224, 255, .5) 100%)",
         backgroundSize: "100%",
         backgroundRepeat: "no-repeat",
         height: "100vh",
@@ -246,7 +246,7 @@ export default async function Page() {
           content={topPosts3}
         ></Feature>
         <Feature
-          linkTo="top=posts"
+          linkTo="top-posts"
           title="Most Popular Posts: for December"
           content={topPosts4}
         ></Feature>
