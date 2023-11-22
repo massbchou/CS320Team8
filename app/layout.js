62ede98884fbfba59629c34034a8435fb8d20706
Nathan G <njgrab135@gmail.com>
2023-11-22 13:22:07 -0500

    Commented out unused var

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 import { Young_Serif } from 'next/font/google'
 
 const youngSerif = Young_Serif({
