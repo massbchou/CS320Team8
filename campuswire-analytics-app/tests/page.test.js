7699dc25325fdf5398191ce6487a6ac1e2f98ae2
Kelly Deng <dengkellyr@gmail.com>
2023-11-24 17:57:16 -0500

    Reformatted tests with describe and it
    2 tests based on correct input aren't passing (may want to reformat code based on them) and 'adds data with correct date range' isn't properly converting dates to times

--- campuswire-analytics-app/tests/page.test.js
+++ campuswire-analytics-app/tests/page.test.js
@@ -1,99 +1,122 @@
 import "@testing-library/jest-dom";
 import { MongoClient } from 'mongodb';
-//const { MongoMemoryServer } = require('mongodb-memory-server');
-const collection = require("./2022-09-28")
 import { formatInput } from '../app/page.js'
 import { extractKeywords } from '../app/page.js'
 import { generateCache } from '../app/page.js'
-
-// const trendingTopics = require("../app/page")
-// trendingTopics.formatInput()
-// trendingTopics.extractKeywords()
-// trendingTopics.generateCache()
+const { PythonShell } = require('python-shell');
 
 const connectionString = 'mongodb+srv://team8s:rattigan320fa23@campuswire.x730pf7.mongodb.net/?retryWrites=true&w=majority';
 let client;
 let database;
-//let collection;
+let collection;
+let dataArray;
 
 beforeAll(async () => { //runs before all tests, establish a connection to the MongoDB server
-  client = new MongoClient(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
-  await client.connect();
+  client = new MongoClient(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+  await client.connect()
 
-  // database = client.db('testing');
-  // collection = database.collection('2022-09-28');
+  database = client.db('testing')
+  collection = database.collection('2022-09-28')
+  dataArray = await collection.find().toArray()
 });
 
 afterAll(async () => { //runs after all tests are done, closes connection
   await client.close();
 });
 
- test('formatInput() removes all private posts', () => {
-  let outputString = formatInput(collection)
-  expect(outputString).toEqual(expect.not.stringContaining('zzz'))
+describe('formatInput', () => {
+  it('removes all private posts', () => {
+    let outputString = formatInput(dataArray)
+    expect(outputString).toEqual(expect.not.stringContaining('zzz'))
+  });
+
+  it('removes all images', () => {
+    let outputString = formatInput(dataArray)
+    expect(outputString).toEqual(expect.not.stringContaining('.png'))
+    expect(outputString).toEqual(expect.not.stringContaining('.jpg'))
+  });
+
+  it('removes all newlines', () => {
+    let outputString = formatInput(dataArray)
+    expect(outputString).toEqual(expect.not.stringContaining('\n'))
+  });
+
+  it('behaves correctly with no input', () => {
+    let outputString = formatInput([])
+    expect(outputString).toEqual("")
+  });
 });
 
-test('formatInput() removes all images', () => {
-  let outputString = formatInput(collection)
-  expect(outputString).toEqual(expect.not.stringContaining('.png'))
-  expect(outputString).toEqual(expect.not.stringContaining('.jpg'))
+describe('extractKeywords', () => {
+  it('outputs keywords with multiple occurrences', () => {
+    let outputString = formatInput(dataArray)
+    let topPhrases = extractKeywords(outputString)
+
+    for (keyword in topPhrases){
+      expect(outputString.split(keyword).length - 1).toBeGreaterThan(1)
+    }
+  });
+
+  it('handles empty string input', async () => {
+    let topPhrases = await extractKeywords("")
+    expect(topPhrases).toEqual([])
+  });
+
+  it('handles empty input', async () => {
+    let topPhrases = await extractKeywords()
+    expect(topPhrases).toEqual([])
+  }); //currently doesn't pass bc ["undefined",] is returned
+
+  it('handles Python script error', async () => {
+    const mockRun = jest.spyOn(PythonShell, 'run').mockRejectedValue(new Error('simulated Python error'));
+    let extractedKeywords;
+    try {
+      extractedKeywords = await extractKeywords(formatInput(dataArray))
+    } catch (error) {
+      expect(error.message).toBe('simulated Python error')
+    } finally {
+      mockRun.mockRestore();
+    }
+    expect(extractedKeywords).toBeUndefined()
+  });
 });
 
-test('formatInput() removes all newlines', () => {
-  let outputString = formatInput(collection)
-  expect(outputString).toEqual(expect.not.stringContaining('\n'))
+describe('generateCache', () => {
+  it('adds data to cache database if its not already there', async () => {
+    let cacheCollection = client.db('testing').collection('test-cache')
+    let postCollection = client.db('testing').collection('2022-09-28')
+    await generateCache(cacheCollection, postCollection, '2022-09-28')
+
+    let cache = await cacheCollection.find()
+    expect(cache).not.toBeNull()
+
+    await cacheCollection.deleteMany({})
+  });
+
+  it('calls with incorrect date format are not added to cache', async () => {
+    let cacheCollection = client.db('testing').collection('test-cache')
+    let postCollection = client.db('testing').collection('2022-09-28')
+    await generateCache(cacheCollection, postCollection, '9-28-22')
+
+    let cache = await cacheCollection.find()
+    expect(cache).toBeNull()
+    await cacheCollection.deleteMany({})
+  });
+
+  it('adds data with correct date range', async () => {
+    let cacheCollection = client.db('testing').collection('test-cache')
+    let postCollection = client.db('testing').collection('2022-10-01')
+    await generateCache(cacheCollection, postCollection, '2022-10-01')
+
+    let comparisonDate = new Date('2022-10-01').getTime() - (1000 * 60 * 60 * 24 * 15);
+    let cache = await cacheCollection.find()
+    let arr = await cache.toArray();
+
+    for (let doc of arr) {
+      let dateInt = new Date(doc.collectionDate).getTime() - (1000 * 60 * 60 * 24 * 15);
+      expect(dateInt).toBeGreaterThan(comparisonDate)
+    }
+
+    await cacheCollection.deleteMany({})
+  });
 });
-
-test('formatInput() no input', () => {
-  let outputString = formatInput([])
-  expect(outputString).toEqual("")
-});
-
-test('extractKeywords() outputs keywords with multiple occurrences', () => {
-  let outputString = formatInput(collection)
-  let topPhrases = extractKeywords(outputString)
-
-  for (keyword in topPhrases){
-    expect(outputString.split(keyword).length - 1).toBeGreaterThan(1)
-  }
-});
-
-// test('extractKeywords() handles Python script error', async () => {
-//   //simulate a Python script error
-//   const pythonScriptError = new Error('Simulated error');
-//   require('python-shell').PythonShell.run.mockRejectedValue(pythonScriptError);
-
-//   let topPhrases = await extractKeywords(formatInput(collection));
-
-//   expect(topPhrases).toBeUndefined();
-// });
-
-test('generateCache() adds data to cache database if its not already there', async () => {
-  let cacheCollection = client.db('testing').collection('test-cache')
-  let postCollection = client.db('testing').collection('2022-09-28')
-  await generateCache(cacheCollection, postCollection, '2022-09-28')
-
-  let cache = await cacheCollection.findOne({collectionDate: '2022-09-28'})
-
-  expect(cache).not.toBeNull()
-
-  await cacheCollection.deleteOne({collectionDate: '2022-09-28'})
-});
-
-// test('generateCache() does not add data to cache database if its already there', async () => {
-//   let cacheCollection = client.db('testing').collection('test-cache')
-//   let postCollection = client.db('testing').collection('2022-09-28')
-
-//   await generateCache(cacheCollection, postCollection, '2022-09-28') //adds to cache
-//   expect(cacheCollection.totalSize()).toBe(1)
-//   await generateCache(cacheCollection, postCollection, '2022-09-28') //same query, should not add anything to the cache
-//   expect(cacheCollection.totalSize()).toBe(1)
-
-//   let cache = await cacheCollection.find({collectionDate: '2022-09-28'});
-//   let post = await cacheCollection.find({collectionDate: '2022-09-28'});
-
-//   expect(cache).toEqual(post)
-//   await cacheCollection.remove({collectionDate: '2022-09-28'})
-// });
-
-// add test for generateCache() correct dates
