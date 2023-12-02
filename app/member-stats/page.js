12ce0b32e335bc8a798afd656325db9024d74cd0
Nathan G <njgrab135@gmail.com>
2023-12-02 16:44:50 -0500

    Integrated real data into the chart for all 5 non-moderator metrics
    - Integrated data for posts, top posts, comments, post views, and unanswered questions on a per-user basis
    - Added interval switching to view the data by week, month, or by semester (all time)

--- app/member-stats/page.js
+++ app/member-stats/page.js
@@ -1,10 +1,14 @@
+import { MongoClient } from "mongodb";
 import Image from "next/image";
 import UserList from "./userlist.js";
 import SelectedUser from "./selected_user.js";
 import StatsGraph from "./stats_graph.js";
 
-export default function Page() {
-    return <main
+export default async function Page() {
+
+  let dataSet = await buildUserDataset();
+
+  return <main
     style={{
       background: "radial-gradient(ellipse at center top, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 50%, rgba(255,255,255,0) 100%), linear-gradient(140deg, rgba(240, 56, 255, .5) 0%, rgba(255,255,255, .5) 50%, rgba(0, 224, 255, .5) 100%)",
       backgroundSize: "cover",
@@ -39,10 +43,10 @@ export default function Page() {
       </span>
     </div>
     <div style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}>
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
     <div style={{
         fontSize: '20px',
     }}>Member Statistics</div>
@@ -63,14 +67,87 @@ export default function Page() {
         title='User #1'
         stats={{numPosts: 5, numTrendingPosts: 2, numUnansweredQuestions: 2, avgReplyTime: 35}}
       ></SelectedUser>
-      <StatsGraph chartData={{
-        numTopPosts: [2, 5, 0, 0, 1, 2, 4],
-        numTrendingPosts: [1, 3, 0, 0, 1, 1, 2],
-        numComments: [4, 2, 0, 0, 1, 2, 3],
-        numPostViews: [20, 30, 15, 0, 50, 0, 0],
-        numUnansweredQuestions: [1, 0, 1, 2, 0, 1, 0],
-      }} isModerator={false}></StatsGraph>
-
+      <StatsGraph data={dataSet.dataArr} startDate={dataSet.startDate} name={dataSet.userName}></StatsGraph>
     </div>
   </main>
+}
+
+async function buildUserDataset(){
+  // initialize MongoClient credentials
+  const url = "mongodb+srv://team8s:rattigan320fa23@campuswire.x730pf7.mongodb.net/?retryWrites=true&w=majority";
+  const client = new MongoClient(url);
+
+  let dataArr = [];
+  let activityStartDate;
+  let name;
+
+  try {
+    await client.connect();
+    // Connect to cluster
+
+    let usersCollection = client.db("users").collection("users");
+
+    const users = usersCollection.find({});
+    let usersObjArr = await users.toArray();
+
+    let userObj = usersObjArr[36];
+
+    let entries = Object.entries(userObj);
+
+    name = userObj.author.firstName + ' ' + userObj.author.lastName;
+    activityStartDate = new Date(entries[2][0]);
+    let activityDate = new Date (activityStartDate);
+
+    let postsCollection = client.db("posts").collection('2022-12-15');
+
+    let i = 2;
+    while(i < entries.length){
+      if(userObj[activityDate.toISOString().substring(0, 10)] !== undefined){
+        let userActivityObj = userObj[activityDate.toISOString().substring(0, 10)];
+
+        let totalViews = 0;
+        let totalUnansweredQuestions = 0;
+        let totalTopPosts = 0;
+        for(let j = 0; j < userActivityObj.postIds.length; j++){
+          const post = await postsCollection.findOne({id: userActivityObj.postIds[j]});
+          totalViews += post.viewsCount;
+          if(post.type === 'question' && !(Object.hasOwn(post, 'modAnsweredAt') || post.comments.reduce((acc, comment) => {acc || comment.endorsed}, false))){
+            totalUnansweredQuestions++;
+          }
+          let postScore = 1 * post.uniqueViewsCount + 2 * (post.viewsCount - post.uniqueViewsCount) + 20 * post.comments.length + 50 * (post.likesCount ? post.likesCount : 0);
+          if(postScore >= 300){
+            totalTopPosts++;
+          }
+        }
+        dataArr.push({
+          numPosts: userActivityObj.postCount,
+          numComments: userActivityObj.commentCount,
+          numPostViews: totalViews,
+          numUnansweredQuestions: totalUnansweredQuestions,
+          numTopPosts: totalTopPosts,
+        });
+        i++;
+      }else{
+        dataArr.push({
+          numPosts: 0,
+          numComments: 0,
+          numPostViews: 0,
+          numUnansweredQuestions: 0,
+          numTopPosts: 0,
+        });
+      }
+      activityDate = new Date(activityDate.getTime() + (24 * 60 * 60 * 1000));
+    }
+
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+
+  return {
+    userName: name,
+    dataArr: dataArr,
+    startDate: activityStartDate,
+  }
 }
--- app/member-stats/stats_graph.js
+++ app/member-stats/stats_graph.js
@@ -1,71 +1,132 @@
 'use client'
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import { Bar } from 'react-chartjs-2'
 import React from 'react';
 import { useState } from 'react';
-import {Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend} from 'chart.js';
-import { Bar } from 'react-chartjs-2'
-import { Young_Serif } from 'next/font/google';
-const youngSerif = Young_Serif({
-    subsets: ['latin'],
-    weight: '400',
-  });
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import Button from '@mui/material/Button';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
 
-
-//ChartJS.defaults.font.family = 'youngSerif';
 ChartJS.defaults.font.size = '15px';
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 export default function StatsGraph(props){
-    let options = {
-        responsive: true,
-        plugins: {
-          legend: {
-            position: 'top',
-          },
-          title: {
-            display: true,
-            text: 'User #1 Statistics',
-          },
-        },
-      };
-      
-      let labels = ['12/9', '12/10', '12/11', '12/12', '12/13', '12/14', '12/15'];
-      
-      let data = {
-        labels,
-        datasets: [
-          {
-            label: 'Top Posts',
-            data: props.chartData.numTopPosts,
-            backgroundColor: 'rgba(0, 224, 255, 0.75)',
-          },
-          {
-            label: 'Trending Posts',
-            data: props.chartData.numTrendingPosts,
-            backgroundColor: 'rgba(0, 178, 255, 0.75)',
-          },
-          {
-            label: 'Comments',
-            data: props.chartData.numComments,
-            backgroundColor: 'rgba(0, 132, 255, 0.75)',
-          },
-          {
-            label: 'Post Views',
-            data: props.chartData.numPostViews,
-            backgroundColor: 'rgba(120, 94, 255, 0.75)',
-          },
-          {
-            label: 'Unanswered Questions',
-            data: props.chartData.numUnansweredQuestions,
-            backgroundColor: 'rgba(240, 56, 255, 0.75)',
-          },
-        ],
-      };
+  const [ currentDayIndex, setCurrentDayIndex ] = useState(0);
+  const [ currentInterval, setInterval ] = useState(7);
+  const [ currentLabels, setLabels ] = useState(generateLabels(props.startDate, currentInterval));
 
-      const [dataSet, setData] = useState(data);
-      const [chartOptions, setOptions] = useState(options);
+  let postsData;
+  let commentsData;
+  let postViewsData;
+  let unansweredQuestionsData;
+  let topPostsData;
 
-      return <div style={{backgroundImage: 'linear-gradient(rgba(0, 224, 255, 0.30), rgba(240, 56, 255, 0.30))', border:'2px', borderStyle:'solid', borderRadius: '10px', padding: '20px', margin: '20px', width: '50%'}}>
-        <Bar options={chartOptions} data={dataSet}/>
-      </div>
+  if(currentDayIndex < 0){
+    postsData = [];
+    commentsData = [];
+    postViewsData = [];
+    unansweredQuestionsData = [];
+    topPostsData = [];
+  }else{
+    postsData = props.data.slice(currentDayIndex, currentDayIndex + currentInterval).map((obj) => obj.numPosts);
+    commentsData = props.data.slice(currentDayIndex, currentDayIndex + currentInterval).map((obj) => obj.numComments);
+    postViewsData = props.data.slice(currentDayIndex, currentDayIndex + currentInterval).map((obj) => obj.numPostViews);
+    unansweredQuestionsData = props.data.slice(currentDayIndex, currentDayIndex + currentInterval).map((obj) => obj.numUnansweredQuestions);
+    topPostsData = props.data.slice(currentDayIndex, currentDayIndex + currentInterval).map((obj) => obj.numTopPosts);
+  }
+
+  let options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Stats for ' + props.name,
+      },
+    },
+  }
+
+  let graphData = {
+    labels: currentLabels.map((x) => x.substring(5, 10)),
+    datasets: [
+      {
+        label: 'Posts',
+        data: postsData,
+        backgroundColor: 'rgba(0, 224, 255, 0.75)',
+      },
+      {
+        label: 'Top Posts',
+        data: topPostsData,
+        backgroundColor: 'rgba(0, 178, 255, 0.75)',
+      },
+      {
+        label: 'Comments',
+        data: commentsData,
+        backgroundColor: 'rgba(0, 132, 255, 0.75)',
+      },
+      {
+        label: 'Post Views',
+        data: postViewsData,
+        backgroundColor: 'rgba(120, 94, 255, 0.75)',
+      },
+      {
+        label: 'Unanswered Questions',
+        data: unansweredQuestionsData,
+        backgroundColor: 'rgba(240, 56, 255, 0.75)',
+      },
+    ],
+  };
+
+  let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  return <div style={{backgroundImage: 'linear-gradient(rgba(0, 224, 255, 0.30), rgba(240, 56, 255, 0.30))', border:'2px', borderStyle:'solid', borderRadius: '10px', padding: '20px', margin: '20px'}}>
+    <Bar width={1000} height={500} options={options} data={graphData}/>
+    <div style={{display:'flex', justifyContent:'space-between'}}>
+      <Button onClick={function(){
+        setCurrentDayIndex(currentDayIndex - currentInterval);
+        setLabels(generateLabels(new Date(new Date(currentLabels[0]).getTime() - (currentInterval * 24 * 60 * 60 * 1000)), currentInterval));
+      }} style={{backgroundColor:'rgba(255, 255, 255, 0.5)', marginTop:'10px'}}
+      ><ChevronLeftIcon/></Button>
+      <div style={{marginTop:'10px'}}>{(currentInterval === 7) ? 'Week of ' + currentLabels[0].substring(5, 10) : (currentInterval === 30) ? months[new Date(currentLabels[0].substring(0, 10)).getMonth()] + ' - ' + months[new Date(currentLabels[currentInterval - 1].substring(0, 10)).getMonth()] : (currentInterval === 99) ? 'All Time' : ''}</div>
+        <Button onClick={function(){
+          setCurrentDayIndex(currentDayIndex + currentInterval);
+          setLabels(generateLabels(new Date(new Date(currentLabels[0]).getTime() + (currentInterval * 24 * 60 * 60 * 1000)), currentInterval));
+        }} style={{backgroundColor:'rgba(255, 255, 255, 0.5)', marginTop:'10px'}}><ChevronRightIcon/></Button>
+  </div>
+  <div style={{display:'flex', justifyContent:'center', alignItems: 'center', margin:'3px'}}>
+    <FormControl>
+      <InputLabel>Interval</InputLabel>
+      <Select
+        labelId="select-interval"
+        value={currentInterval}
+        label="Interval"
+        onChange={(event) => {
+          setInterval(event.target.value);
+          setLabels(generateLabels(new Date(currentLabels[0]).getTime(), event.target.value));
+        }}
+      >
+        <MenuItem value={7}>Weekly</MenuItem>
+        <MenuItem value={30}>Monthly</MenuItem>
+        <MenuItem value={99}>All Time</MenuItem>
+      </Select>
+    </FormControl>
+  </div>
+</div>
+}
+
+function generateLabels(startingDate, interval){
+  let newLabels = [];
+
+  for(let i = 0; i < interval; i++){
+    newLabels.push(new Date(new Date(startingDate).getTime() + i * (24 * 60 * 60 * 1000)).toISOString().substring(0, 10));
+  }
+
+  return newLabels;
 }
