4b55f725db5136975237aca9bbfdad4c2fad0b8e
johncle <90406181+johncle@users.noreply.github.com>
2023-12-07 00:32:50 -0500

    update loading spinner

--- app/loading.js
+++ app/loading.js
@@ -1,4 +1,4 @@
-import "./loading.css";
+import { CircularProgress } from "@mui/material";
 
 export default function Loading() {
   return (
@@ -10,9 +10,23 @@ export default function Loading() {
         backgroundrepeat: "no-repeat",
         width: "100%",
         height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <div className="spinner">loading</div>;
+      <svg style={{ position: "absolute" }}>
+        <defs>
+          <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+            <stop offset="0%" stopColor="rgba(0, 224, 255, 1)" />
+            <stop offset="100%" stopColor="rgba(240, 56, 255, 1)" />
+          </linearGradient>
+        </defs>
+      </svg>
+      <CircularProgress
+        sx={{ "svg circle": { stroke: "url(#gradient)" } }}
+        size={60}
+      />
     </div>
   );
 }
