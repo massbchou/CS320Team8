1061da71c46b1fedb7e661206fd1e0193e41f736
Nathan G <njgrab135@gmail.com>
2023-12-07 22:41:51 -0500

    Added user switching within the user list
    Each button has an href in the form:
    `href={"/member-stats?userID=" + user.id}`
    The page takes in that parameter with `props.searchParams.userID` and builds the selected user window and the stats graph using it

--- app/member-stats/userlist.js
+++ app/member-stats/userlist.js
@@ -8,21 +8,16 @@ const youngSerif = Young_Serif({
   });
 // Returns a component for a list of users in alphabetical order
 export default function UserList(props){
-
-    let selectedUser = 0;
-
     return (
         <div style={{backgroundImage: 'linear-gradient(rgba(0, 224, 255, 0.45), rgba(240, 56, 255, 0.55))', borderRadius: '10px', padding: '20px', margin: '20px', width: '18%', height: '70vh'}}>
             <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
-                <div style={{textAlign:'center', fontFamily: youngSerif, fontSize:'20px', backgroundColor:'rgba(255, 255, 255, 0.70)', borderRadius:'10px', width:'100%', padding:'6px', marginBottom:'9px', marginRight:'9px'}}>{props.title}</div>
-                {(props.totalCount === undefined) ? '' : <div style={{textAlign:'center', fontFamily:'Young Serif', fontSize:'20px', backgroundColor:'rgba(255, 255, 255, 0.70)', borderRadius:'10px', width:'20%', padding:'6px', marginBottom:'9px'}}>{props.totalCount}</div>}
+                <div style={{textAlign:'center', fontFamily: youngSerif, fontSize:'20px', backgroundColor:'rgba(255, 255, 255, 0.70)', borderRadius:'10px', width:'100%', padding:'6px', marginBottom:'9px', marginRight:'9px'}}>Forum Users</div>
+                <div style={{textAlign:'center', fontFamily:'Young Serif', fontSize:'20px', backgroundColor:'rgba(255, 255, 255, 0.70)', borderRadius:'10px', width:'20%', padding:'6px', marginBottom:'9px'}}>{props.userList.length}</div>
             </div>
             <div style={{display:'flex', justifyContent:'center', maxHeight:'90%', overflow:'auto',}}>
                 <List component="nav" sx={{display:'flex', flexDirection:'column', width:'85%'}}>
-                    {props.users.map((user) => (
-                        <ListItemButton key={user.id} 
-                            onClick={() => props.onUserSelect(user.id)}
-                        >
+                    {props.userList.map((user) => (
+                        <ListItemButton style={{fontFamily: youngSerif, fontSize:'17px', textAlign:'center', margin:'6px', padding:'2px', backgroundColor:'rgba(255, 255, 255, .9)', opacity:'0.6', borderRadius:'7px'}} href={"/member-stats?userID=" + user.id} key={user.id}>
                             <ListItemText primary={user.name}/>
                         </ListItemButton>
                     ))}
