96678c63c423e28edf17bb181111710eda71a395
Nathan G <njgrab135@gmail.com>
2023-12-09 19:04:52 -0500

    Added moderator tags and a return home button
    - Added moderator tags for all mods in the user list and the selected user window
    - Both moderator stats are colored blue and hidden for non-mods
    - Added a return home button to the top of the page

--- app/member-stats/page.js
+++ app/member-stats/page.js
@@ -48,10 +48,19 @@ export default async function Page(props) {
       display: "flex",
       justifyContent: "center",
       alignItems: "center",
+      flexDirection: "column",
     }}>
     <div style={{
         fontSize: '20px',
     }}>Member Statistics</div>
+    <a href="/" style={{display: 'inline-block', marginTop:'7px'}}>
+      Return Home
+      <div style={{display: 'inline-block'}}>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="-2 -2 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+      </svg>
+      </div>
+    </a>
     </div>
     <div
       style={{
@@ -79,6 +88,7 @@ async function buildUserDataset(userID){
   let dataArr = [];
   let activityStartDate;
   let name;
+  let userRole;
 
   try {
     await client.connect();
@@ -92,6 +102,8 @@ async function buildUserDataset(userID){
     }else{
       userObj = await usersCollection.findOne({});
     }
+
+    userRole = userObj.author.role;
 
     let entries = Object.entries(userObj);
 
@@ -150,6 +162,7 @@ async function buildUserDataset(userID){
     userName: name,
     dataArr: dataArr,
     startDate: activityStartDate,
+    userRole: userRole,
   }
 }
 
@@ -163,13 +176,14 @@ async function buildUserList(){
   try {
     await client.connect();
     const usersCollection = client.db("users").collection("users");
-    const usersCursor = usersCollection.find({}, { projection: { "author.firstName": 1, "author.lastName": 1, "author.id": 1 } });
+    const usersCursor = usersCollection.find({}, { projection: { "author.firstName": 1, "author.lastName": 1, "author.id": 1, "author.role": 1} });
     const usersList = await usersCursor.toArray();
 
     // Build an array of user names and their IDs
     users = usersList.map(user => ({
       name: user.author.firstName + ' ' + user.author.lastName,
       id: user.author.id,
+      role: user.author.role,
     }));
   } catch (e) {
     console.error(e);
--- app/member-stats/selected_user.js
+++ app/member-stats/selected_user.js
@@ -7,8 +7,9 @@ const youngSerif = Young_Serif({
 export default function SelectedUser(props) {
     return (
         <div style={{backgroundImage: 'linear-gradient(rgba(0, 224, 255, 0.45), rgba(240, 56, 255, 0.55))', borderRadius: '10px', padding: '20px', margin: '20px', width: '25%', height: '70vh'}}>
-            <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
-                <div style={{textAlign:'center', fontFamily: youngSerif, fontSize:'20px', backgroundColor:'rgba(255, 255, 255, 0.70)', borderRadius:'10px', width:'100%', padding:'6px', marginBottom:'9px', marginRight:'9px'}}>{props.userDataset.userName}</div>
+            <div style={{display:'flex', justifyContent:'center', alignItems:'center', backgroundColor:'rgba(255, 255, 255, 0.70)', borderRadius:'10px', marginBottom:'16px'}}>
+                <div style={{textAlign:'center', fontFamily: youngSerif, fontSize:'20px', width:'100%', padding:'6px'}}>{props.userDataset.userName}</div>
+                {(props.userDataset.userRole === 'moderator') ? <div style={{textAlign:'center', borderRadius:'15px', backgroundColor:'rgba(0, 132, 255, .6)', opacity:'0.8', width:'50%', marginRight:'20px'}}>Moderator</div> : null}
             </div>
             <div style={{display:'flex', justifyContent:'center', alignItems:'center', marginTop:'7px'}}>
                 <span style={{fontFamily: youngSerif, fontSize:'17px', textAlign:'center', margin:'6px', padding:'2px', backgroundColor:'rgba(255, 255, 255, 1)', opacity:'0.6', borderRadius:'7px', width:'85%'}}>Number of posts: </span>
@@ -30,14 +31,15 @@ export default function SelectedUser(props) {
                 <span style={{fontFamily: youngSerif, fontSize:'17px', textAlign:'center', margin:'6px', padding:'2px', backgroundColor:'rgba(255, 255, 255, 1)', opacity:'0.6', borderRadius:'7px', width:'85%'}}>Number of unanswered questions: </span>
                 <span style={{fontFamily: youngSerif, fontSize:'17px', textAlign:'center', margin:'6px', padding:'2px', backgroundColor:'rgba(255, 255, 255, 1)', opacity:'0.6', borderRadius:'7px', width:'15%'}}>{props.userDataset.dataArr.reduce((acc, postData) => acc + postData.numUnansweredQuestions, 0)}</span>
             </div>
+            {(props.userDataset.userRole === 'moderator') ? <div>
             <div style={{display:'flex', justifyContent:'center', alignItems:'center', marginTop:'7px'}}>
-                <span style={{fontFamily: youngSerif, fontSize:'17px', textAlign:'center', margin:'6px', padding:'2px', backgroundColor:'rgba(255, 255, 255, 1)', opacity:'0.6', borderRadius:'7px', width:'85%'}}>Average response time (in minutes): </span>
-                <span style={{fontFamily: youngSerif, fontSize:'17px', textAlign:'center', margin:'6px', padding:'2px', backgroundColor:'rgba(255, 255, 255, 1)', opacity:'0.6', borderRadius:'7px', width:'15%'}}>{0}</span>
+                <span style={{fontFamily: youngSerif, fontSize:'17px', textAlign:'center', margin:'6px', padding:'2px', backgroundColor:'rgba(0, 132, 255, .7)', opacity:'0.6', borderRadius:'7px', width:'85%'}}>Average response time (in minutes): </span>
+                <span style={{fontFamily: youngSerif, fontSize:'17px', textAlign:'center', margin:'6px', padding:'2px', backgroundColor:'rgba(0, 132, 255, .7)', opacity:'0.6', borderRadius:'7px', width:'15%'}}>{0}</span>
             </div>
             <div style={{display:'flex', justifyContent:'center', alignItems:'center', marginTop:'7px'}}>
-                <span style={{fontFamily: youngSerif, fontSize:'17px', textAlign:'center', margin:'6px', padding:'2px', backgroundColor:'rgba(255, 255, 255, 1)', opacity:'0.6', borderRadius:'7px', width:'85%'}}>Number of first respondent incidents: </span>
-                <span style={{fontFamily: youngSerif, fontSize:'17px', textAlign:'center', margin:'6px', padding:'2px', backgroundColor:'rgba(255, 255, 255, 1)', opacity:'0.6', borderRadius:'7px', width:'15%'}}>{0}</span>
-            </div>
+                <span style={{fontFamily: youngSerif, fontSize:'17px', textAlign:'center', margin:'6px', padding:'2px', backgroundColor:'rgba(0, 132, 255, .7)', opacity:'0.6', borderRadius:'7px', width:'85%'}}>Number of first respondent incidents: </span>
+                <span style={{fontFamily: youngSerif, fontSize:'17px', textAlign:'center', margin:'6px', padding:'2px', backgroundColor:'rgba(0, 132, 255, .7)', opacity:'0.6', borderRadius:'7px', width:'15%'}}>{0}</span>
+            </div></div> : null}
         </div>
     )
 }
